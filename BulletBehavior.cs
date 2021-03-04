d325fdd44aa216309d5a9fd8a19049a05a217175
Samin-Sadman-Shad <70101913+Samin-Sadman-Shad@users.noreply.github.com>
2021-03-05 01:45:15 +0600

    Add files via upload

---
+++ BulletBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class BulletBehavior : MonoBehaviour
+{
+    //Rigidbody bulletRb;
+    //public GameObject target;
+    //public GameObject gun;
+    //public GameObject bulletPrefab;
+
+    public float forceValue;
+    public GameObject gameManager;
+    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Target"))
+        {
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+            GameManager.score++;
+            GameManager.scoreText.SetText("Score: " + GameManager.score);
+            //Debug.Log("the score is " + GameManager.score);
+        }
+        else if(collision.gameObject.CompareTag("Special Target"))
+        {
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+            GameManager.gameOver = true;
+        }
+    }
+
+    public void ThrowBullet(Vector3 targetPosition, Vector3 gunPointerPosition)
+    {
+        gameObject.SetActive(true);
+        Rigidbody bulletRb = gameObject.GetComponent<Rigidbody>();
+        //Debug.Log("bullet should move now ");
+        //Debug.DrawRay(gunPointerPosition, (targetPosition - gunPointerPosition));
+        //Debug.DrawRay(gunPointerPosition, (targetPosition - gunPointerPosition));
+        bulletRb.AddForce((targetPosition - gunPointerPosition) * forceValue, ForceMode.Impulse);
+        //gameObject.transform.Translate((targetPosition - gunPointerPosition) * Time.deltaTime , Space.World);
+    }
+}
