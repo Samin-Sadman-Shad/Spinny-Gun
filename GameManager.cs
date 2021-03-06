ffaf71446888cddf42fba9127fc09f53376be323
Samin-Sadman-Shad <70101913+Samin-Sadman-Shad@users.noreply.github.com>
2021-03-06 21:48:37 +0600

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public static int ammoNumberPerLevel = 10;
+    
+    public static int level;
+    public static int ammoNumber;
+    public static int score;
+
+    PathFollower[] targets;
+    public GameObject gun;
+    
+    public static event Action KeyPressed;
+    public static event Action NewLevel;
+
+    public static TextMeshProUGUI scoreText;
+    public static TextMeshProUGUI LevelText;
+    public static TextMeshProUGUI AmmoText;
+    public static TextMeshProUGUI gameOverText;
+
+    public static bool gameOver;
+    public static bool specialLevel;
+    TextMeshProUGUI[] allText;
+
+    void Awake()
+    {
+        level = 0;
+        score = 0;
+        //scoreText = FindObjectOfType<TextMeshProUGUI>();
+        gun = FindObjectOfType<Gun>().gameObject;
+        allText = FindObjectsOfType<TextMeshProUGUI>();
+        for(int i=0; i < allText.Length; i++)
+        {
+            if (allText[i].CompareTag("ScoreText"))
+            {
+                scoreText = allText[i];
+            }
+            else if (allText[i].CompareTag("Level"))
+            {
+                LevelText = allText[i];
+            }
+            else if (allText[i].CompareTag("Ammo"))
+            {
+                AmmoText = allText[i];
+            }
+            else if (allText[i].CompareTag("GameOver"))
+            {
+                gameOverText = allText[i];
+            }
+
+        }
+
+        scoreText.SetText("Score: " + score);
+        ammoNumber = ammoNumberPerLevel + level;
+        GameManager.AmmoText.SetText("Ammo: " + GameManager.ammoNumber);
+        NewLevel += ChangeAmmoNumber;
+        //Debug.Log("the score is " + score);
+    }
+
+    void ChangeAmmoNumber()
+    {
+        ammoNumber = ammoNumberPerLevel + level;
+        GameManager.AmmoText.SetText("Ammo: " + GameManager.ammoNumber);
+        //Debug.Log("total bullets are " + ammoNumber);
+    }
+
+   
+    void Update()
+    {
+        targets = FindObjectsOfType<PathFollower>();
+        if(targets.Length == 0)
+        {
+            level++;
+            NewLevel();
+            LevelText.SetText("Level: " + level);
+            //Debug.Log("level is " + level);
+            //Debug.Log(NewLevel.GetInvocationList().Length);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            KeyPressed();
+            /*
+            Delegate[] keyPressedList = KeyPressed.GetInvocationList();
+            for (int i=0; i< KeyPressed.GetInvocationList().Length; i++)
+            {
+                Debug.Log(keyPressedList[i]);
+            }
+            */
+        }
+        //Debug.Log(ammoNumber);
+        
+        if(ammoNumber < 1 || gameOver)
+        {
+            //Debug.Log("Game Over");
+            PathFollower[] targets = FindObjectsOfType<PathFollower>();
+            for(int i=0; i<targets.Length; i++)
+            {
+                if(targets[i].movingSpeed > 0)
+                {
+                    targets[i].movingSpeed -= Time.deltaTime * 5;
+                }
+            }
+
+            if (gun.activeInHierarchy)
+            {
+                gun.SetActive(false);
+            }
+            gameOverText.SetText("Game Over");
+        }
+        
+    }
+}
---
+++ TargetSpawner.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetSpawner : MonoBehaviour
+{
+    public GameObject sampleTarget;
+    public static GameObject specialTarget;
+    public Texture texture1;
+    public Texture texture2;
+    public Texture texture3;
+
+    public Node[] path;
+
+    public int currentNumberOfTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        path = Path.path;
+        //Debug.Log(path.Length);
+        GameManager.NewLevel += TargetSpawn;
+    }
+
+
+     public GameObject[] Spawn(int n)
+     {
+        //float xPosition = Random.Range(path[0].transform.position.x, path[3].transform.position.x);
+        //float yPosition = Random.Range(path[2].transform.position.y, path[3].transform.position.y);
+        GameObject[] targets = new GameObject[n];
+
+        Vector3 offset1 = new Vector3(-0.1f, 0, 0);
+        Vector3 offset2 = new Vector3(0, -2, 0);
+
+        Vector3 spawnPos1 = path[7].transform.position + offset1;
+        Vector3 spawnPos2 = path[1].transform.position + offset2;
+        //Vector3 spawningPos1 = sampleTarget.transform.position
+
+        //if(n % 5 != 0)
+        //{
+            //Debug.Log(n);
+            //Debug.Log(n % 5);
+            for (int i = 0; i < n; i++)
+            {
+                if (i == 0)
+                {
+                    targets[i] = Instantiate(sampleTarget, sampleTarget.transform.position, sampleTarget.transform.rotation);
+                    targets[i].GetComponent<PathFollower>().SetCurrentNodeIndex(0);
+                    targets[i].GetComponent<MeshRenderer>().material.mainTexture = texture3;
+                }
+                else if (i == 1)
+                {
+                    targets[i] = Instantiate(sampleTarget, spawnPos1, sampleTarget.transform.rotation);
+                    targets[i].GetComponent<PathFollower>().SetCurrentNodeIndex(0);
+                    targets[i].GetComponent<MeshRenderer>().material.mainTexture = texture3;
+                }
+                else if (i == 2)
+                {
+                    targets[i] = Instantiate(sampleTarget, spawnPos2, sampleTarget.transform.rotation);
+                    targets[i].GetComponent<PathFollower>().RotateTargetPrimary();
+                    targets[i].GetComponent<PathFollower>().SetCurrentNodeIndex(2);
+                    targets[i].GetComponent<MeshRenderer>().material.mainTexture = texture3;
+                }
+                else if (i == 3)
+                {
+                    targets[i] = Instantiate(sampleTarget, spawnPos2 + offset2, sampleTarget.transform.rotation);
+                    targets[i].GetComponent<PathFollower>().RotateTargetPrimary();
+                    targets[i].GetComponent<PathFollower>().SetCurrentNodeIndex(2);
+                    targets[i].GetComponent<MeshRenderer>().material.mainTexture = texture3;
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+        //}
+        
+        if (n % 5 == 0)
+        {
+            GameManager.specialLevel = true;
+            //Debug.Log(n);
+            //Debug.Log("special level");
+            PathFollower[] targetComponent = FindObjectsOfType<PathFollower>();
+            /*
+            for (int i = 0; i < targetComponent.Length; i++)
+            {
+                Destroy(targetComponent[i].gameObject);
+            }
+            */
+
+            specialTarget = Instantiate(sampleTarget, spawnPos1, sampleTarget.transform.rotation);
+            specialTarget.tag = "Special Target";
+            if (n % 10 == 0)
+            {
+                //specialTarget = Instantiate(sampleTarget, spawnPos1, sampleTarget.transform.rotation);
+                specialTarget.GetComponent<MeshRenderer>().material.mainTexture = texture1 ;
+            }
+            else
+            {
+                specialTarget.GetComponent<MeshRenderer>().material.mainTexture = texture2;
+            }
+            for(int i=0; i<targets.Length; i++)
+            {
+                if(targets[i].transform.position == specialTarget.transform.position)
+                {
+                    Destroy(targets[i]);
+                }
+            }
+        }
+
+        return targets;
+     }
+
+    public void TargetSpawn()
+    {
+         Spawn(GameManager.level);
+        //Debug.Log("level is " + GameManager.level);
+        //TargetSpawnSingle();
+    }
+
+    public GameObject TargetSpawnSingle()
+    {
+       GameObject singleTarget = Instantiate(sampleTarget, sampleTarget.transform.position, sampleTarget.transform.rotation);
+        return singleTarget;
+    }
+}
